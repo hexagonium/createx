d9c954f0d3ad0fc038f48210aee3cffd8ed11231
hexagonium <selfeduc1234@gmail.com>
2023-04-06 01:21:20 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-Fancybox.bind();
+$(function () {
+    Fancybox.bind();
+
+    var mixer = mixitup('.c-cards');
+
+    $('.c-filter__button').on('click', function name(event) {
+        $('.c-filter__button').removeClass('c-filter__button_active')
+        $(this).addClass('c-filter__button_active')
+    })
+})
