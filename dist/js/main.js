85a1e29b82146b9a227076f4798c79e16e48d3e7
hexagonium <selfeduc1234@gmail.com>
2023-04-06 22:32:42 +0500

    Свёрстан блок с сертификатом

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,13 @@
 $(function () {
     Fancybox.bind();
 
-    var mixer = mixitup('.c-cards');
+    var config = {
+        animation: {
+            duration: 300,
+            effects: "fade"
+        }
+    }
+    var mixer = mixitup('.c-cards', config);
 
     $('.c-filter__button').on('click', function name(event) {
         $('.c-filter__button').removeClass('c-filter__button_active')
